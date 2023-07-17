40f3dee42edc7cfe535cc6ce79eeb941a9375b20
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-07-17 12:10:33 -0300

    refactor: change store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,14 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-import { configureStore, MiddlewareArray } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
-import thunk from 'redux-thunk'
-import multi from 'redux-multi'
-import promise from 'redux-promise'
 
 import rootReducers from './reducers'
 import { pokemonApi } from './services/pokemon'
 
 export const store = configureStore({
   reducer: rootReducers,
-  middleware: new MiddlewareArray()
-    .concat(thunk, multi, promise, pokemonApi.middleware)
+  middleware: getDefaultMiddliware =>
+    getDefaultMiddliware()
+      .concat(pokemonApi.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
