bf8cdb62008fe4aaa0069d7b82ab9c35de62721d
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-07-17 11:22:56 -0300

    refactor: add spacing top in header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,7 @@
 import Logo from '../../assets/icons/pokedex.svg'
 
 const Header = () => (
-  <header className="container mx-auto flex justify-center w-full max-w-screen-2xl">
+  <header className="container mx-auto flex justify-center w-full max-w-screen-2xl mt-14">
     <img src={Logo} alt="Logo Pokedex" className='mr-2 w-32 h-32' />
     <h1 className='text-9xl text-white'>Pokedex</h1>
   </header>
